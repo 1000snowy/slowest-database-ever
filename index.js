7fc036954a801e26cb9385ba49493d869736c5f2
Snowy ✨#1000 <karliyimamk@gmail.com>
2021-08-11 12:13:17 +0300

    Delete method is done

--- index.js
+++ index.js
@@ -16,38 +16,59 @@ module.exports = class Database {
             let nested = object;
 
             while (nested != null && ++index < length) {
-                const data = key[index]
-                let newValue = value
+                const data = key[index];
+                let newValue = value;
 
                 if (index != lastIndex) {
-                    const objValue = nested[data]
-                    newValue = (typeof objValue === "object") ? objValue : {}
-                }
+                    const objValue = nested[data];
+                    newValue = (typeof objValue === "object") ? objValue : {};
+                };
                 nested[data] = newValue;
                 nested = nested[data];
-            }
+            };
 
-            return object;
+            return (index && index == length) ? object : undefined;
         };
 
-        fs.writeFileSync(this.filename, JSON.stringify(this.database));
-        return set(key, value, this.database, this.filename);
+        fs.writeFileSync(this.filename, JSON.stringify(set(key, value, this.database)));
+        return set(key, value, this.database);
     };
 
     get(key) {
-        function get(path, object) {
-            path = path.split(".")
+        function get(key, object) {
+            key = key.split(".");
+            const length = key.length;
 
-            let index = 0
-            const length = path.length
+            let index = 0;
 
             while (object != null && index < length) {
-                object = object[path[index++]]
-            }
+                object = object[key[index++]];
+            };
 
-            return (index && index == length) ? object : undefined
+            return (index && index == length) ? object : undefined;
         };
 
         return get(key, this.database);
-    }
+    };
+
+    delete(key) {
+        function unset(key, object) {
+            let prop = key.split(".");
+            let propLength = prop.length;
+            let current = object;
+            let i = 0;
+            while (i < propLength) {
+                if (i === propLength - 1) {
+                    delete current[prop[i]];
+                    return object;
+                } else {
+                    current = current[prop[i]];
+                }
+                i++;
+            }
+        }
+
+        fs.writeFileSync(this.filename, JSON.stringify(unset(key, this.database)));
+        return unset(key, this.database);
+    };
 };
